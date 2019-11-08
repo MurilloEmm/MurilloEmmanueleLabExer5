d9a5f5fd2df48197ca947d6b39d33be12a26853c
Emmanuele Murillo <2016094282@ust-ics.mygbiz.com>
2019-11-08 21:29:53 +0800

    yes

--- app/src/main/java/com/murillo/oct25/MainActivity.java
+++ app/src/main/java/com/murillo/oct25/MainActivity.java
@@ -5,21 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.murillo.oct25.R.layout.newlayout;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    String[] verName,dates, ceo, industry, description;
-    int[] logo = {R.drawable.icb, R.drawable.jp, R.drawable.ccb, R.drawable.abc, R.drawable.boa, R.drawable.ap,R.drawable.ping,R.drawable.boc,R.drawable.shel,R.drawable.wf,R.drawable.exm,R.drawable.att,R.drawable.sam, R.drawable.cg};
+    String[] verName, dates, ceo, industry, description;
+    int[] logo = {R.drawable.icb, R.drawable.jp, R.drawable.ccb, R.drawable.abc, R.drawable.boa, R.drawable.ap, R.drawable.ping, R.drawable.boc, R.drawable.shel, R.drawable.wf, R.drawable.exm, R.drawable.att, R.drawable.sam, R.drawable.cg};
     ListView lstVersions;
     ArrayList<AndroidVersion> versions = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +35,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ceo = getResources().getStringArray(R.array.ceoN);
         industry = getResources().getStringArray(R.array.indN);
         description = getResources().getStringArray(R.array.description);
-        for(int i = 0; i < verName.length; i++){
+        for (int i = 0; i < verName.length; i++) {
             versions.add(new AndroidVersion(logo[i], verName[i], dates[i], ceo[i], industry[i], description[i]));
         }
 
-        AndroidAdapter adapter = new AndroidAdapter(this, R.layout.newlayout ,versions);
+        AndroidAdapter adapter = new AndroidAdapter(this, R.layout.newlayout, versions);
         lstVersions = findViewById(R.id.lvAndroid);
         lstVersions.setAdapter(adapter);
         lstVersions.setOnItemClickListener(this);
@@ -41,30 +47,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
-
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-        Toast.makeText(this,verName[i], Toast.LENGTH_LONG).show();
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setIcon(logo[i]);
-        dialog.setTitle(verName[i]);
-        dialog.setMessage(description[i]);
-        dialog.setNeutralButton("Close", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this, verName[i], Toast.LENGTH_LONG).show();
+        final File folder = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(folder, "company.txt");
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            String choice = verName[i];
+            fos.write(choice.getBytes());
+            Toast.makeText(this, verName[i], Toast.LENGTH_LONG).show();
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setIcon(logo[i]);
+            dialog.setTitle(verName[i]);
+            dialog.setMessage(description[i]);
+            dialog.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    Toast.makeText(MainActivity.this, description[i], Toast.LENGTH_LONG).show();
 
-            }
+                }
 
-        });
-
-
+            });
 
 
+            dialog.create().show();
 
-
-        dialog.create().show();
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
